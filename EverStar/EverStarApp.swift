83f4971fe3221a0391e22162fb707b26b894516e
bachnxt <bach071196@gmail.com>
2022-11-16 10:39:18 +0700

    Initial Commit

---
+++ EverStar/EverStarApp.swift
@@ -0,0 +1,17 @@
+//
+//  EverStarApp.swift
+//  EverStar
+//
+//  Created by bach on 16/11/2022.
+//
+
+import SwiftUI
+
+@main
+struct EverStarApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
